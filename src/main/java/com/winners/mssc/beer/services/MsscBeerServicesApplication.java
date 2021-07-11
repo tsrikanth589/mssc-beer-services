c9762d2b20ddb49a65f51db2a6a8692032b2c60e
SRIKANTH <SRIKANTH@HP>
2021-07-12 00:13:40 +0530

    Initial Spring Starter Project with 3 added data model classes
    1.BeerDto
    2.BeerStyleEnum
    3.BeerPagedList

---
+++ src/main/java/com/winners/mssc/beer/services/MsscBeerServicesApplication.java
@@ -0,0 +1,13 @@
+package com.winners.mssc.beer.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServicesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/winners/mssc/beer/services/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.winners.mssc.beer.services.web.model;
+
+public enum BeerStyleEnum {
+	LAGER,PILSNER,STOUT,GOSE,PORTER,ALE,WHEAT,IPA,PALE_ALE,SAISON
+}
